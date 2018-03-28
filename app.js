05837e2bb63552959516baa573e7d2fe1418ed70
zhaoyunxing92 <2385585770@qq.com>
2018-03-28 16:18:20 +0800

    修改：使用套壳方式嵌入web

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 App({
  
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    // 显示动态数字
+    wx.setTabBarBadge({
+      index: 2,
+      text: '9+'
+    })
     // 登录
     wx.login({
       success: res => {
