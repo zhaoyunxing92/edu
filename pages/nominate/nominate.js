5d3f198d9a517f7dfb550db60a9d7c5b2ffe4d17
zhaoyunxing92 <2385585770@qq.com>
2018-03-29 17:20:31 +0800

    添加：推荐页面

--- pages/nominate/nominate.js
+++ pages/nominate/nominate.js
@@ -1,4 +1,4 @@
-// pages/search/search.js
+// pages/nominate/nominate.js
 Page({
 
   /**
@@ -12,16 +12,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-   
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    wx.setNavigationBarTitle({
-      title: '搜索'
-    })
+  
   },
 
   /**
