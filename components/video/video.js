def6159cf44e49e8fd98ab390b95acccf9d5eaf4
zhaoyunxing92 <2385585770@qq.com>
2018-03-30 01:37:53 +0800

    添加：播放组件

--- components/video/video.js
+++ components/video/video.js
@@ -1,66 +1,76 @@
-// components/video/video.js
-Page({
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   * 用于组件自定义设置
+   */
+  properties: {
+    initialTime:{// 开始时间
+      type: String,
+      value: '0'
+    },
+    url:{//播放地址
+      type: String,
+    },
+    danmuList: {//弹幕列表
+      type: Array,
+      value: []
+    },
+    // 视频封面
+    cover: { 
+      type: String,
+      value: '取消'
+    },
+    // 弹窗确认按钮文字
+    confirmText: {
+      type: String,
+      value: '确定'
+    }
+  },
 
   /**
-   * 页面的初始数据
+   * 私有数据,组件的初始数据
+   * 可用于模版渲染
    */
   data: {
-  
+    // 弹窗显示控制
+    isShow: false
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 组件的方法列表
+   * 更新属性和数据的方法与更新页面数据的方法类似
    */
-  onLoad: function (options) {
-  
-  },
+  methods: {
+    /*
+     * 公有方法
+     */
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    //隐藏弹框
+    hideDialog() {
+      this.setData({
+        isShow: !this.data.isShow
+      })
+    },
+    //展示弹框
+    showDialog() {
+      this.setData({
+        isShow: !this.data.isShow
+      })
+    },
+    /*
+    * 内部私有方法建议以下划线开头
+    * triggerEvent 用于触发事件
+    */
+    _cancelEvent() {
+      //触发取消回调
+      this.triggerEvent("cancelEvent")
+    },
+    _confirmEvent() {
+      //触发成功回调
+      this.triggerEvent("confirmEvent");
+    }
   }
 })
