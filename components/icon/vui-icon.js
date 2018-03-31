3098bdceeec5f6c7582774a8c3cb9df9b30a269c
zhaoyunxing92 <2385585770@qq.com>
2018-03-31 17:58:41 +0800

    添加：icon组件

---
+++ components/icon/vui-icon.js
@@ -0,0 +1,18 @@
+// components/icon/vui-icon.js
+Component({
+  properties: {
+    // icon名称
+    name: {
+      type: String,
+      value: ""
+    },
+    color: {
+      type: String,
+      value: '#666',
+    },
+    size: {
+      type: String,
+      value: '21',
+    }
+  },
+})
