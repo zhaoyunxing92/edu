02ebc51e4fa05c04a53fa57421bb94fff690fb34
zhaoyunxing92 <2385585770@qq.com>
2018-03-31 15:37:51 +0800

    添加：卡片、课程列表

---
+++ components/courseList/vui-course-list.js
@@ -0,0 +1,27 @@
+// components/courseList/vui-course-list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 课程
+    courses: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
