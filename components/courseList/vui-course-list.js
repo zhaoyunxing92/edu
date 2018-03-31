4195f6fe45e894747bc6941f29b40192f257da04
zhaoyunxing92 <2385585770@qq.com>
2018-03-31 16:39:12 +0800

    优化：课程组件列表

--- components/courseList/vui-course-list.js
+++ components/courseList/vui-course-list.js
@@ -22,6 +22,13 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    //跳转
+    _navigateTo(env){
+      let path = env.currentTarget.dataset.path;
+      let item = env.currentTarget.dataset.item;
+      wx.navigateTo({
+        url: path + '?item=' + JSON.stringify(item)
+      })
+    }
   }
 })
